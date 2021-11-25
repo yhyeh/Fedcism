0043d3f4a3957bb3f39d00b301cb8618b8a32a96
Yu-Hsuan Yeh <yhyeh.cs09@nycu.edu.tw>
2021-11-25 20:52:57 +0800

    Add participant selection with statistical utility

--- main_slct.py
+++ main_slct.py
@@ -26,18 +26,14 @@ if __name__ == '__main__':
 
     base_dir = './save/{}/{}_iid{}_num{}_C{}_le{}/shard{}/{}/'.format(
         args.dataset, args.model, args.iid, args.num_users, args.frac, args.local_ep, args.shard_per_user, args.results_save)
-    if not os.path.exists(os.path.join(base_dir, 'fed')):
-        os.makedirs(os.path.join(base_dir, 'fed'), exist_ok=True)
+    if not os.path.exists(os.path.join(base_dir, 'statsel')):
+        os.makedirs(os.path.join(base_dir, 'statsel'), exist_ok=True)
 
     dataset_train, dataset_test, dict_users_train, dict_users_test = get_data(args)
-    '''
     print('type: ', type(dataset_test))
     print('len: ', len(dataset_test))
-    local_data_size = []
-    for idx in range(args.num_users):
-        local_data_size.append(len(dict_users_train[idx]))
-    print('local dataset size: ', local_data_size.sort())
-    '''
+    
+
     shard_path = './save/{}/{}_iid{}_num{}_C{}_le{}/shard{}/'.format(
         args.dataset, args.model, args.iid, args.num_users, args.frac, args.local_ep, args.shard_per_user)
     dict_save_path = os.path.join(shard_path, 'shared_dict_users.pkl')
@@ -50,13 +46,18 @@ if __name__ == '__main__':
         with open(dict_save_path, 'wb') as handle:
             pickle.dump((dict_users_train, dict_users_test), handle)
         
+    local_data_size = []
+    for idx in range(args.num_users):
+        local_data_size.append(len(dict_users_train[idx]))
+    print('local dataset size: ', local_data_size)
+
     # build model
     net_glob = get_model(args)
     net_glob.train()
-    #print(list(net_glob.layer_hidden1.weight)[0])
 
     # training
-    results_save_path = os.path.join(base_dir, 'fed/results.csv')
+    results_save_path = os.path.join(base_dir, 'statsel/results.csv')
+    slctcnt_save_path = os.path.join(base_dir, 'statsel/selection_cnt.csv')
 
     loss_train = []
     time_train = []
@@ -68,6 +69,9 @@ if __name__ == '__main__':
     lr = args.lr
     results = []
 
+    epsilon = 0.5 # exploitation rate
+    slct_cnt = np.zeros(args.num_users) 
+
     for iter in range(args.epochs):
         t_geps_bgin = time.time()
         time_locals = []
@@ -75,16 +79,37 @@ if __name__ == '__main__':
         w_glob = None
         loss_locals = []
         m = max(int(args.frac * args.num_users), 1) # num of selected clients
-        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        
+        if iter == 0: # first round
+            idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+        else:
+            '''
+            keep top good_cli = m*epsilon utility client
+            drop & random select (1-good_cli)
+            '''
+            sorted_u = {k: v for k, v in sorted(utility.items(), key=lambda item: item[1], reverse=True)}
+            print('utility', sorted_u)
+            kept = []
+            n_exploi = round(m*epsilon)
+            n_explor = m - round(m*epsilon)
+            for k, v in sorted(utility.items(), key=lambda item: item[1], reverse=True):
+                kept.append(k)
+                if len(kept) == n_exploi:
+                    break
+            print('kept', kept)
+            rest_pool = list(set(range(args.num_users)) - set(idxs_users))
+            rest_sel = np.random.choice(rest_pool, n_explor, replace=False)
+            idxs_users = np.concatenate((kept, rest_sel))
 
-
-        print("Round {}, lr: {:.6f}, {}".format(iter, lr, idxs_users))
+        print("\n Round {}, lr: {:.6f}, {}".format(iter, lr, idxs_users))
         '''
         for idx in idxs_users:
             print('user: ', idx, '==================================')
             print('dict_users_train[idx]: ', type(dict_users_train[idx]), len(dict_users_train[idx]))
             #print(dict_users_train[idx])
         '''
+
+        utility = {} # clientID : utility
         
         for idx in idxs_users: # iter over selected clients
             t_leps_bgin = time.time()
@@ -96,7 +121,9 @@ if __name__ == '__main__':
             loss_locals.append(copy.deepcopy(loss))
             # loss: a float, avg loss over local epochs over batches
             #print('loss: ', loss)
-
+            B_i = len(dict_users_train[idx])
+            utility[idx] = np.sqrt(B_i*loss**2)
+            slct_cnt[idx] += 1
 
             if w_glob is None:
                 w_glob = copy.deepcopy(w_local)
@@ -138,18 +165,20 @@ if __name__ == '__main__':
                 best_epoch = iter
 
             # if (iter + 1) > args.start_saving:
-            #     model_save_path = os.path.join(base_dir, 'fed/model_{}.pt'.format(iter + 1))
+            #     model_save_path = os.path.join(base_dir, 'statsel/model_{}.pt'.format(iter + 1))
             #     torch.save(net_glob.state_dict(), model_save_path)
 
             results.append(np.array([iter, loss_avg, loss_test, acc_test, best_acc, time_local_avg, time_glob]))
             final_results = np.array(results)
             final_results = pd.DataFrame(final_results, columns=['epoch', 'loss_avg', 'loss_test', 'acc_test', 'best_acc', 'time_local_avg', 'time_glob'])
             final_results.to_csv(results_save_path, index=False)
+            np.savetxt(slctcnt_save_path, slct_cnt, delimiter=",")
 
         if (iter + 1) % 50 == 0:
-            best_save_path = os.path.join(base_dir, 'fed/best_{}.pt'.format(iter + 1))
-            model_save_path = os.path.join(base_dir, 'fed/model_{}.pt'.format(iter + 1))
+            best_save_path = os.path.join(base_dir, 'statsel/best_{}.pt'.format(iter + 1))
+            model_save_path = os.path.join(base_dir, 'statsel/model_{}.pt'.format(iter + 1))
             torch.save(net_best.state_dict(), best_save_path)
             torch.save(net_glob.state_dict(), model_save_path)
 
     print('Best model, iter: {}, acc: {}'.format(best_epoch, best_acc))
+    

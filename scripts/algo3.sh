97896f6f97976031ed02b7e13a8a082bd718ed38
Yu-Hsuan Yeh <yhyeh.cs09@nycu.edu.tw>
2022-06-02 20:27:00 +0800

    Update training scripts

--- scripts/algo3.sh
+++ scripts/algo3.sh
@@ -1,33 +1,94 @@
 LR=0.1
 LE=1
-EP=1000
+EP=500
+DATA=cifar10
+NCLS=10
+E=0.8
+DEG=1
+DIR=cossim_zipf_vi
 
-#for RUN in cls_imb; do
-for RUN in cossim_zipf; do
+#for RUN in cossim_test; do
 
-  # main_slct.py , adaptive gamma w.r.t. loss
-  for ALGO in 3; do
-    for FRAC in 0.1 0.05; do
-      for DEG in 1; do
-        for E in 0.8 0.5; do
+for VI in 3; do
+  for RUN in 2; do
+
+    for FRAC in 0.05 0.1; do
+      
+      # shard 10 fed
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+        --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+        --results_save ${DIR}${VI}_r${RUN} \
+        --cls_imb --vol_imb ${VI} --clsimb_type zipf \
+        --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl
+      
+      # shard 2 fed
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+        --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+        --results_save ${DIR}${VI}_r${RUN} \
+        --cls_imb  --vol_imb ${VI} --clsimb_type zipf \
+        --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl
+      
+      for ALGO in 3 0; do
         
-          
-          # shard 10 fed w/ sel
-          python3 main_slct_algo3.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
-            --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-            --results_save ${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
-            --cls_imb --data_distr zipf_dict_users.pkl \
-            --epsilon ${E} --clsimb_type zipf
-          
+        # shard 10 fed w/ sel
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+          --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+          --results_save ${DIR}${VI}_r${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
+          --cls_imb --vol_imb ${VI} --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl \
+          --epsilon ${E} --clsimb_type zipf \
+          --gpu 2
         
-          # shard 2 fed w/ sel
-          python3 main_slct_algo3.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
-            --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-            --results_save ${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
-            --cls_imb --data_distr zipf_dict_users.pkl \
-            --epsilon ${E} --clsimb_type zipf
+        # shard 2 fed w/ sel
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+          --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+          --results_save ${DIR}${VI}_r${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
+          --cls_imb --vol_imb ${VI} --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl \
+          --epsilon ${E} --clsimb_type zipf \
+          --gpu 2
+      
+      done
+    done
+  done
+done
+
+
+for VI in 5; do
+  for RUN in 1 2; do
+
+    for FRAC in 0.05 0.1; do
+      
+      # shard 10 fed
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+        --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+        --results_save ${DIR}${VI}_r${RUN} \
+        --cls_imb --vol_imb ${VI} --clsimb_type zipf \
+        --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl
+      
+      # shard 2 fed
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+        --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+        --results_save ${DIR}${VI}_r${RUN} \
+        --cls_imb  --vol_imb ${VI} --clsimb_type zipf \
+        --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl
+      
+      for ALGO in 3 0; do
         
-        done
+        # shard 10 fed w/ sel
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+          --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+          --results_save ${DIR}${VI}_r${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
+          --cls_imb --vol_imb ${VI} --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl \
+          --epsilon ${E} --clsimb_type zipf \
+          --gpu 2
+        
+        # shard 2 fed w/ sel
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
+          --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
+          --results_save ${DIR}${VI}_r${RUN} --myalgo ${ALGO} --deg ${DEG} --wndw_size 100 \
+          --cls_imb --vol_imb ${VI} --data_distr zipf_dict_users_vi${VI}_r${RUN}.pkl \
+          --epsilon ${E} --clsimb_type zipf \
+          --gpu 2
+      
       done
     done
   done
--- scripts/slct_cifar.sh
+++ scripts/slct_cifar.sh
@@ -1,49 +1,54 @@
 LR=0.1
 LE=1
 EP=1000
-for RUN in cossim_zipf; do
+DATA=cifar10
+NCLS=10
+DIR=cossim_htail_vi
+
+for VI in 3 1 5; do
   for ALGO in 0; do
     for FRAC in 0.1 0.05; do
       
-      :'
+      
       # shard 10 fed
-      python3 main_fed.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
         --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-        --results_save ${RUN} \
-        --cls_imb  --clsimb_type zipf \
-        --data_distr zipf_dict_users.pkl  
+        --results_save ${DIR}${VI} \
+        --cls_imb --vol_imb ${VI} --clsimb_type htail \
+        --data_distr htail_dict_users_vi${VI}.pkl
       
       # shard 2 fed
-      python3 main_fed.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
+      python3 main_fed.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
         --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-        --results_save ${RUN} \
-        --cls_imb  --clsimb_type zipf \
-        --data_distr zipf_dict_users.pkl  
-      '
-
-      for E in 0.5 0.8; do
+        --results_save ${DIR}${VI} \
+        --cls_imb  --vol_imb ${VI} --clsimb_type htail \
+        --data_distr htail_dict_users_vi${VI}.pkl
+      
+      :'
+      for E in 0.8; do
 
       
         # shard 10 oort
-        python3 main_slct_algo3.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
           --num_users 100 --shard_per_user 10 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-          --results_save ${RUN} --myalgo ${ALGO} \
+          --results_save ${DIR} --myalgo ${ALGO} \
           --cls_imb  --clsimb_type zipf \
           --data_distr zipf_dict_users.pkl \
-          --epsilon ${E}   
+          --epsilon ${E}
         
-        :'
+        
         # shard 2 oort
-        python3 main_slct_algo3.py --dataset cifar10 --model cnn --num_classes 10 --epochs ${EP} --lr ${LR} \
+        python3 main_slct_algo3.py --dataset ${DATA} --model cnn --num_classes ${NCLS} --epochs ${EP} --lr ${LR} \
           --num_users 100 --shard_per_user 2 --frac ${FRAC} --local_ep ${LE} --local_bs 50 \
-          --results_save ${RUN}  --myalgo ${ALGO} \
+          --results_save ${DIR}  --myalgo ${ALGO} \
           --cls_imb  --clsimb_type zipf \
           --data_distr zipf_dict_users.pkl \
-          --epsilon ${E}    
-        '
+          --epsilon ${E}
+        
       
       
       done
+      '
     done
   done
 done
